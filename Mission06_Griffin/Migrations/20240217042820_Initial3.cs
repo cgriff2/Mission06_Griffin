4ebeb93d830c9f3d2ed8f3f43597c07180ddc3b4
Charlotte Griffin <charlottegriffin11@gmail.com>
2024-02-16 21:32:22 -0700

    Movies added, checking database

---
+++ Mission06_Griffin/Migrations/20240217042820_Initial3.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission06_Griffin.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Applications",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LentTo",
+                table: "Applications",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Edited",
+                table: "Applications",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LentTo",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Edited",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- Mission06_Griffin/Models/Application.cs
+++ Mission06_Griffin/Models/Application.cs
@@ -8,8 +8,8 @@
         public int Year { get; set; }
         public string Director { get; set; }
         public string Rating { get; set; }
-        public string Edited { get; set; }
-        public string LentTo { get; set; }
-        public string Notes { get; set; }
+        public string? Edited { get; set; }
+        public string? LentTo { get; set; }
+        public string? Notes { get; set; }
     }
 }
